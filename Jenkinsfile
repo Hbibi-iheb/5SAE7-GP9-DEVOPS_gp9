178c289e9cdeb820608d0fc0033432f563e9fb69
sahra <mohamedsahraoui.guesmi@esprit.tn>
2024-11-03 12:47:18 +0100

    project sski

---
+++ Jenkinsfile
@@ -0,0 +1,84 @@
+pipeline {
+    agent any
+    tools {
+        maven 'M2_HOME'
+    }
+    stages {
+        stage('GIT') {
+            steps {
+                git branch: 'Mohamed_Sahraoui_Guesmi_5sae7',
+                    url: 'https://github.com/Hbibi-iheb/5SAE7-GP9-DEVOPS_gp9.git',
+                    credentialsId: 'jenkins-example-github-pat'
+            }
+        }
+   
+       stage('build and test ')
+        { steps{
+            script{
+            sh " mvn clean install -X -DscriptTests=true"
+            sh " mvn test"}
+        }
+        }
+    stage('maven build') {
+steps {
+    script {
+        sh "mvn package -DscriptTests=true"
+    }
+}
+    }
+    stage('SonarQube Scanner') {
+            steps {
+                
+                    withSonarQubeEnv('sonarqube') {
+                 script{
+                 
+                    sh "mvn sonar:sonar"
+                 }
+
+
+                }
+                
+            }
+        }
+        stage('nexus'){
+            steps {
+                script {
+                  sh ' mvn deploy  '
+                }
+            }
+        }
+              
+          stage('Build Docker Image') {
+            steps {
+                script {
+                    try{
+                    sh 'mvn clean package -DscriptTests'
+                    sh 'docker build -t sahraoui44/ski-devops:1.0.0 .'
+                    } catch(e){
+                     echo "Docker build failed: ${e}"
+                        currentBuild.result = 'FAILURE' 
+                        error("Docker image build failed")
+                    }
+                }
+            
+        }
+         }
+               stage('Deploy Docker Image') {
+            steps {
+             
+                script {
+                 withCredentials([string(credentialsId: 'dockerhub-pwd', variable: 'dockerhubpwd')]) {
+                    sh 'docker login -u sahraoui44 -p ${dockerhubpwd}'
+                 }  
+                 sh 'docker push sahraoui44/ski-devops:1.0.0'
+                }
+            }
+        }
+         stage('Deploy with Docker Compose') {
+            steps {
+                sh 'docker-compose up -d'
+            }
+        } 
+        
+    }
+}
---
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -0,0 +1,88 @@
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.spring.entities.Piste;
+import tn.esprit.spring.repositories.IPisteRepository;
+import tn.esprit.spring.services.PisteServicesImpl;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class GestionStationSkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Mock
+    private IPisteRepository pisteRepository;
+
+    @InjectMocks
+    private PisteServicesImpl pisteServices;
+
+    @Test
+    public void testRetrieveAllPistes() {
+        // Arrange
+        Piste piste1 = new Piste();
+        Piste piste2 = new Piste();
+        Mockito.when(pisteRepository.findAll()).thenReturn(Arrays.asList(piste1, piste2));
+
+        // Act
+        List<Piste> pistes = pisteServices.retrieveAllPistes();
+
+        // Assert
+        Assertions.assertEquals(2, pistes.size());
+        Mockito.verify(pisteRepository, Mockito.times(1)).findAll();
+    }
+
+    @Test
+    public void testAddPiste() {
+        // Arrange
+        Piste piste = new Piste();
+        Mockito.when(pisteRepository.save(piste)).thenReturn(piste);
+
+        // Act
+        Piste savedPiste = pisteServices.addPiste(piste);
+
+        // Assert
+        Assertions.assertNotNull(savedPiste);
+        Mockito.verify(pisteRepository, Mockito.times(1)).save(piste);
+    }
+
+    @Test
+    public void testRemovePiste() {
+        // Arrange
+        Long pisteId = 1L;
+        Mockito.doNothing().when(pisteRepository).deleteById(pisteId);
+
+        // Act
+        pisteServices.removePiste(pisteId);
+
+        // Assert
+        Mockito.verify(pisteRepository, Mockito.times(1)).deleteById(pisteId);
+    }
+
+    @Test
+    public void testRetrievePiste() {
+        // Arrange
+        Long pisteId = 1L;
+        Piste piste = new Piste();
+        Mockito.when(pisteRepository.findById(pisteId)).thenReturn(Optional.of(piste));
+
+        // Act
+        Piste retrievedPiste = pisteServices.retrievePiste(pisteId);
+
+        // Assert
+        Assertions.assertNotNull(retrievedPiste);
+        Mockito.verify(pisteRepository, Mockito.times(1)).findById(pisteId);
+    }
+
+}
